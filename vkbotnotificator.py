2de181daea9b5015821152d08cd25aecaf273113
Vitaly Bogryakov <vitjrbog@gmail.com>
2017-11-08 19:47:29 +0500

    Upgrade
    - now all data storing in one JSON file;
    - custom path for storing file with data;
    - a separately functions for reading and writing files;
    - listening of activity in few communities;
    - full name of author of post in top a message;
    - and few minor changes.

--- vkbotnotificator.py
+++ vkbotnotificator.py
@@ -1,8 +1,107 @@
-# coding: utf8
+# coding: utf-8
 
 
 import vk_api
 import time
+import os
+import json
+import copy
+import urllib
+
+def starter():
+
+    try:
+
+        if os.path.exists("path.txt") is False:
+            file_text = open("path.txt", "w")
+            file_text.write("")
+            file_text.close()
+            print("COMPUTER: Was created file \"path.txt\".")
+
+        PATH = read_path()
+
+        if os.path.exists(PATH + "data.json") is False:
+            print("\nCOMPUTER: WARNING! File \"data.json\" not found!")
+
+            data_json = {
+                "publics": [],
+                "admin_token": "",
+                "bot_token": ""
+            }
+
+            write_json("Starter", PATH, "data", data_json)
+
+            print("COMPUTER: Please, enter the necessary data to " +
+                  "file \"data.json\". Exit from program...")
+            exit(0)
+
+        #  Файлы с токеном, временная мера
+
+        data_json = read_json("Starter", PATH, "data")
+
+        vk_admin_token = data_json["admin_token"]
+        vk_bot_token = data_json["bot_token"]
+
+        data_access_admin = {
+            "token": vk_admin_token
+        }
+        data_access_bot = {
+            "token": vk_bot_token
+        }
+
+        vk_admin_session = autorization(data_access_admin, "token")
+        vk_bot_session = autorization(data_access_bot, "token")
+
+        main(vk_admin_session, vk_bot_session)
+
+    except Exception as var_except:
+        print(
+            "COMPUTER: Error, " + str(var_except) + ". Exit from program...")
+        exit(0)
+
+
+def read_path():
+    try:
+        path = str(open("path.txt", "r").read())
+
+        if len(path) > 0 and path[len(path) - 1] != "/":
+            path += "/"
+
+        return path
+
+    except Exception as var_except:
+        print(
+            "COMPUTER [.. -> Read \"path.txt\"]: Error, " + str(var_except) +
+            ". Exit from program...")
+        exit(0)
+
+
+def read_json(sender, path, file_name):
+    try:
+        loads_json = json.loads(open(str(path) + str(file_name) +
+                                     ".json", 'r').read())  # dict
+
+        return loads_json
+    except Exception as var_except:
+        print(
+            "COMPUTER [.. -> " + str(sender) +
+            " -> Read JSON]: Error, " + str(var_except) +
+            ". Exit from program...")
+        exit(0)
+
+
+def write_json(sender, path, file_name, loads_json):
+    try:
+        file_json = open(str(path) + str(file_name) + ".json", "w")
+        file_json.write(json.dumps(loads_json, indent=4, ensure_ascii=False))
+        file_json.close()
+
+    except Exception as var_except:
+        print(
+            "COMPUTER [.. -> " + str(sender) +
+            " -> Write JSON]: Error, " + str(var_except) +
+            ". Exit from program...")
+        exit(0)
 
 
 def autorization(data_access, auth_type):
@@ -30,63 +129,57 @@ def autorization(data_access, auth_type):
         return vk_session
 
 
-def starter():
+def main(vk_admin_session, vk_bot_session):
 
-    #  Токен аккаунта 404386296. Временная мера
-    vk_user_token = "b4e7aa2e35dd290404eef768d72132e1454b9d" +\
-                    "2150b7ad960c12f4bbf52d56dfcf88685cd7cafa25fe7fc"
-
-    data_access = {
-        "token": vk_user_token
-    }
-
-    vk_session = autorization(data_access, "token")
-
-    main(vk_session)
-
-
-def main(vk_session):
-
-    vk_adminroom_token = "fec0fef8222c5a09d76b44674ca3fef0eecc602647" +\
-                         "defb2978a5ee03ddf7da886bc35629f35ebd0f77781"
-
-
-    last_date = int(open("last_date.txt", "r").read())
+    PATH = read_path()
 
     # Вечный цикл
 
     while True:
 
-        response = get_post(vk_session, -61061413)
+        data_json = read_json("Main", PATH, "data")
 
-        i = len(response["items"]) - 1
+        publics = copy.deepcopy(data_json["publics"])
 
-        while i >= 0:
+        i = 0
 
-            item = response["items"][i]
+        while i < len(publics):
 
-            if item["date"] > last_date:
+            response = get_post(vk_admin_session, publics[i]["id"])
 
-                # ПЕРЕД ПРОДАКШЕНОМ ПОМЕНЯТЬ vk_adminroom_token
+            j = len(response["items"]) - 1
 
-                last_date = send_message(vk_adminroom_token, item, 404386296, last_date)
+            while j >= 0:
 
-                file_text = open("last_date.txt", "w")
-                file_text.write(str(last_date))
-                file_text.close()
+                item = response["items"][j]
 
-                print(str("New last date: " + str(last_date)))
+                last_date = int(publics[i]["last_date"])
 
-                time.sleep(1)
+                if item["date"] > last_date:
 
-            i -= 1
+                    last_date = send_message(vk_bot_session, item,
+                                             publics[i]["id"], 
+                                             last_date)
+
+                    data_json["publics"][i]["last_date"] = str(last_date)
+
+                    write_json("Main", PATH, "data", data_json)
+
+                    print(str("New last date " + publics[i]["name"] +
+                              ": " + str(last_date)))
+
+                    time.sleep(1)
+
+                j -= 1
+
+            i += 1
 
         # Задержка на 60 секунд
 
         time.sleep(60)
 
 
-def get_post(vk_session, owner_id):
+def get_post(vk_admin_session, owner_id):
 
     values = {
         'owner_id': int(owner_id),
@@ -94,43 +187,49 @@ def get_post(vk_session, owner_id):
         'filter': "suggests"
     }
 
-    response = vk_session.method("wall.get", values)
+    response = vk_admin_session.method("wall.get", values)
 
     return response
 
 
-def send_message(access_token, item, send_to, last_date):
+def send_message(vk_bot_session, item, send_to, last_date):
+
+    text = ""
+
+    if len(item["text"]) > 1000:
+        text = item["text"][0:1000] + "... \n [long text]"
+    else:
+        text = item["text"]
 
     post_info = {
-            "text": item["text"],
+            "text": text,
             "from_id": item["from_id"],
             "id": item["id"],
             "owner_id": item["owner_id"]
         }
 
-    author = str(post_info["from_id"])
+    response_author = vk_bot_session.method("users.get", {
+                                            "user_ids": item["from_id"]})
+
+    first_name = response_author[0]["first_name"]
+    last_name = response_author[0]["last_name"]
+
+    author_full_name = first_name + " " + last_name
+
+    author_url = "*id" + str(item["from_id"]) + " (" + author_full_name + ")"
+
     id_post = str(post_info["owner_id"]) + "_" + str(post_info["id"])
 
-    text_post = post_info["text"] +\
-                "\n|----------" +\
-                "\n|" + "https://vk.com/id" + author +\
-                "\n|" + "https://vk.com/wall" + id_post +\
-                "\n|----------"
+    text_post = author_url + "\n" +\
+                "\n" + post_info["text"] + "\n" +\
+                "\n" + "https://vk.com/wall" + id_post
 
     values = {
             "user_id": send_to,
             "message": text_post
         }
 
-
-    data_access = {
-        "token": access_token
-    }
-
-    #  Авторизация
-    vk_message_session = autorization(data_access, "token")
-
-    vk_message_session.method("messages.send", values)
+    vk_bot_session.method("messages.send", values)
 
     return item["date"]
 

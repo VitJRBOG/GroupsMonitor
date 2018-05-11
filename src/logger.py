22d465ca4d7aafe6f9c9155ccc6fb1b1885e7d1d
Vitaly Bogryakov <vitjrbog@gmail.com>
2018-05-11 20:24:45 +0500

    Refactor
    - вывод текста в обработчиках исключений теперь производиться через общую функцию вывода.

--- src/logger.py
+++ src/logger.py
@@ -7,62 +7,58 @@ import time
 def exception_handler(sender, var_except):
     try:
         if str(var_except).lower().find("captcha needed") != -1:
-            print(
-                "COMPUTER [" + sender + "]: Error, " +
-                str(var_except) + ". " +
-                "Timeout: 60 sec.")
+            message = "Error, " +\
+                str(var_except) + ". " +\
+                "Timeout: 60 sec."
+            message_output(sender, message)
             time.sleep(60)
 
             return
-
         elif str(var_except).lower().find("failed to establish " +
                                           "a new connection") != -1:
-            print(
-                "COMPUTER [" + sender + "]: Error, " +
-                str(var_except) + ". " +
-                "Timeout: 60 sec.")
+            message = "Error, " +\
+                str(var_except) + ". " +\
+                "Timeout: 60 sec."
+            message_output(sender, message)
             time.sleep(60)
 
             return
-
         elif str(var_except).lower().find("connection aborted") != -1:
-            print(
-                "COMPUTER [" + sender + "]: Error, " +
-                str(var_except) + ". " +
-                "Timeout: 60 sec.")
+            message = "Error, " +\
+                str(var_except) + ". " +\
+                "Timeout: 60 sec."
+            message_output(sender, message)
             time.sleep(60)
 
             return
         elif str(var_except).lower().find("response code 504") != -1:
-            print(
-                "COMPUTER [" + sender + "]: Error, " +
-                str(var_except) + ". " +
-                "Timeout: 60 sec.")
+            message = "Error, " +\
+                str(var_except) + ". " +\
+                "Timeout: 60 sec."
+            message_output(sender, message)
             time.sleep(60)
 
             return
         elif str(var_except).lower().find("response code 502") != -1:
-            print(
-                "COMPUTER [" + sender + "]: Error, " +
-                str(var_except) + ". " +
-                "Timeout: 60 sec.")
+            message = "Error, " +\
+                str(var_except) + ". " +\
+                "Timeout: 60 sec."
+            message_output(sender, message)
             time.sleep(60)
 
             return
-
         else:
-            print(
-                "COMPUTER [" + sender + "]: Error, " +
-                str(var_except) +
-                ". Exit from program...")
+            message = "Error, " +\
+                str(var_except) +\
+                ". Exit from program..."
+            message_output(sender, message)
             exit(0)
 
     except Exception as var_except:
         sender += " -> Exception handler"
-        print(
-            "COMPUTER [" + sender + "]: Error, " +
-            str(var_except) +
-            ". Exit from program...")
+        message = "Error, " +\
+            str(var_except) +\
+            ". Exit from program..."
         exit(0)
 
 

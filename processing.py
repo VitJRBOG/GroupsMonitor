7a77c19119f7d0ef5a0f37b10c3f0791bb8908ab
Vitaly Bogryakov <vitjrbog@gmail.com>
2018-12-01 14:06:59 +0500

    Upgrade - исправлен баг с отсутствием вызова функции обновления невалидного токена; - исправлен баг с отсутствием обновления токена в файле данных.

--- processing.py
+++ processing.py
@@ -39,15 +39,30 @@ def check_access_tokens():
             output_data.output_text_row(sender, message)
             return vk_session
         except Exception as message_error:
-            if message_error == "invalid access_token":
-                message = token_owner + "'s access token is invalid."
+            if str(message_error).lower().find("invalid access_token") > -1:
+                message = token_owner + "'s access token is invalid. Need another..."
+                output_data.output_text_row(sender, message)
                 access_token = request_new_access_token(token_owner)
+                update_access_token(token_owner, access_token)
                 return check_session(token_owner, access_token)
 
     def request_new_access_token(token_owner):
         u"""Запрос нового токена доступа."""
         access_token = input_data.get_vk_user_token(token_owner)
         return access_token
+
+    def update_access_token(token_owner, access_token):
+        u"""Обновляет токен доступа в файле с данными."""
+        PATH = data_manager.read_path()
+        dict_data = data_manager.read_json(PATH, "data")
+        if token_owner == "Admin":
+            dict_data["admin_access_token"] = access_token
+        else:
+            subjects = dict_data["subjects"]
+            for i, subject in enumerate(subjects):
+                if subject["name"] == token_owner:
+                    dict_data["subjects"][i]["sender_access_token"] = access_token
+        data_manager.write_json(PATH, "data", dict_data)
 
     PATH = data_manager.read_path()
     dict_data = data_manager.read_json(PATH, "data")

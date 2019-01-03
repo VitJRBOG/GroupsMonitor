08551875f1125e35409cf4efbb63003885112155
Vitaly Bogryakov <vitjrbog@gmail.com>
2019-01-03 11:34:13 +0500

    Update - описан свой модуль отправки запросов к VK API.

---
+++ vkapi.py
@@ -0,0 +1,22 @@
+# coding: utf8
+u"""Модуль формирования запросов к VK API."""
+
+
+import requests
+
+
+def method(method_name, values, access_token):
+    u"""Отправка запроса к VK API."""
+    request = "https://api.vk.com/method/"
+
+    request += method_name
+    request += "?v=" + str(values["v"])
+    request += "&access_token=" + access_token
+
+    keys_values = values.keys()
+    for key in keys_values:
+        request += "&" + key + "=" + str(values[key])
+
+    response = requests.post(request)
+
+    return response.text

9cf22436ebb08bc554c111e7c94864458d6623c2
Vitaly Bogryakov <vitjrbog@gmail.com>
2019-04-02 15:41:22 +0500

    Initial commit

--- input_data.py
+++
@@ -1,9 +0,0 @@
-# coding: utf8
-u"""Модуль ввода данных пользователем."""
-
-
-def get_vk_user_token(token_owner, token_purpose):
-    u"""Ввод пользователем токена доступа в консоль."""
-    access_token = raw_input("USER [" + token_owner +\
-        "'s new access token for " + token_purpose + "]: ")
-    return access_token

5c39058b79eb93f13a51df0c2c06454cc9864bec
Vitaly Bogryakov <vitjrbog@gmail.com>
2019-04-21 18:21:52 +0500

    Update - изменен текст в выводимых сообщениях.

--- input_data.go
+++ input_data.go
@@ -6,7 +6,7 @@ import (
 
 // InputNameAccessToken принимает ввод имени токена доступа пользователем в консоль и возвращает его
 func InputNameAccessToken() (string, error) {
-	fmt.Print("\n> [Name of access token for update]: ")
+	fmt.Print("> [Name of access token for update]: ")
 	var userAnswer string
 	_, err := fmt.Scan(&userAnswer)
 	if err != nil {
@@ -17,7 +17,7 @@ func InputNameAccessToken() (string, error) {
 
 // InputAccessToken принимает ввод токена доступа пользователем в консоль и возвращает его
 func InputAccessToken(name string) (string, error) {
-	fmt.Printf("\n> [New access token for %v]: ", name)
+	fmt.Printf("> [New access token for %v]: ", name)
 	var userAnswer string
 	_, err := fmt.Scan(&userAnswer)
 	if err != nil {

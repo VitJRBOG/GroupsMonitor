b27b0e0bcac93172adc7271f272b1a54b6d941df
Vitaly Bogryakov <vitjrbog@gmail.com>
2019-04-22 15:37:46 +0500

    Refactor - функции модуля мониторинга постов вынесены в отдельный файл.

---
+++ wall_post_monitor.go
@@ -0,0 +1,274 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+// WallPostMonitor проверяет посты со стены
+func WallPostMonitor(subject Subject) error {
+	sender := fmt.Sprintf("%v's wall post monitoring", subject.Name)
+
+	// запрашиваем структуру с параметрами модуля мониторинга постов
+	wallPostMonitorParam, err := SelectDBWallPostMonitorParam(subject.ID)
+	if err != nil {
+		return err
+	}
+
+	// запрашиваем структуру с постами со стены субъекта
+	wallPosts, err := getWallPosts(sender, subject, wallPostMonitorParam)
+	if err != nil {
+		return err
+	}
+
+	var targetWallPosts []WallPost
+
+	// отфильтровываем старые
+	for _, wallPost := range wallPosts {
+		if wallPost.Date > wallPostMonitorParam.LastDate {
+			targetWallPosts = append(targetWallPosts, wallPost)
+		}
+	}
+
+	// если после фильтрации что-то осталось, продолжаем
+	if len(targetWallPosts) > 0 {
+
+		// сортируем в порядке от раннего к позднему
+		for j := 0; j < len(targetWallPosts); j++ {
+			f := 0
+			for i := 0; i < len(targetWallPosts)-1-j; i++ {
+				if targetWallPosts[i].Date > targetWallPosts[i+1].Date {
+					x := targetWallPosts[i]
+					y := targetWallPosts[i+1]
+					targetWallPosts[i+1] = x
+					targetWallPosts[i] = y
+					f = 1
+				}
+			}
+			if f == 0 {
+				break
+			}
+		}
+
+		// перебираем отсортированный список
+		for _, wallPost := range targetWallPosts {
+
+			// формируем строку с данными для карты для отправки сообщения
+			messageParameters, err := makeMessageWallPost(sender, subject, wallPostMonitorParam, wallPost)
+			if err != nil {
+				return err
+			}
+
+			// отправляем сообщение с полученными данными
+			if err := SendMessage(sender, messageParameters, subject); err != nil {
+				return err
+			}
+
+			// обновляем дату последнего проверенного поста в БД
+			if err := UpdateDBWallPostMonitorLastDate(subject.ID, wallPost.Date, wallPostMonitorParam); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+// WallPost хранит данные о посте со стены
+type WallPost struct {
+	ID          int          `json:"id"`
+	OwnerID     int          `json:"owner_id"`
+	FromID      int          `json:"from_id"`
+	Date        int          `json:"date"`
+	PostType    string       `json:"post_type"`
+	Text        string       `json:"text"`
+	Attachments []Attachment `json:"attachments"`
+}
+
+// getWallPosts формирует запрос на получение постов со стены и посылает его к vk api
+func getWallPosts(sender string, subject Subject, wallPostMonitorParam WallPostMonitorParam) ([]WallPost, error) {
+	var wallPosts []WallPost
+
+	// формируем карту с параметрами запроса
+	jsonDump := fmt.Sprintf(`{
+			"owner_id": "%d",
+			"count": "%d",
+			"filter": "%v",
+			"v": "5.95"
+		}`, subject.SubjectID, wallPostMonitorParam.PostsCount, wallPostMonitorParam.Filter)
+	values, err := MakeJSON(jsonDump)
+	if err != nil {
+		return wallPosts, err
+	}
+
+	// отправляем запрос, получаем ответ
+	response, err := SendVKAPIQuery(sender, "wall.get", values, subject)
+	if err != nil {
+		return wallPosts, err
+	}
+
+	// парсим полученные данные о постах
+	wallPosts = parseWallPostsVkAPIMap(response["response"].(map[string]interface{}))
+
+	return wallPosts, nil
+}
+
+func parseCopyHistory(copyHistory []map[string]interface{}) Attachment {
+	var attachment Attachment
+
+	// извлекаем данные об одном единственном репосте из карты vk api
+	attachment.Type = copyHistory[0]["post_type"].(string)
+	attachment.OwnerID = copyHistory[0]["owner_id"].(int)
+	attachment.ID = copyHistory[0]["id"].(int)
+	if accessKey, exist := copyHistory[0]["access_key"]; exist {
+		attachment.AccessKey = accessKey.(string)
+	}
+	return attachment
+}
+
+// parseWallPostsVkAPIMap извлекает данные о постах из полученной карты vk api
+func parseWallPostsVkAPIMap(resp map[string]interface{}) []WallPost {
+	var wallPosts []WallPost
+
+	// перебираем элементы с данными о постах
+	itemsMap := resp["items"].([]interface{})
+	for _, itemMap := range itemsMap {
+		item := itemMap.(map[string]interface{})
+
+		// парсим данные из элемента в структуру
+		var wallPost WallPost
+		wallPost.ID = int(item["id"].(float64))
+		wallPost.OwnerID = int(item["owner_id"].(float64))
+		if _, exist := item["signer_id"]; exist == true {
+			wallPost.FromID = int(item["signer_id"].(float64))
+		} else {
+			wallPost.FromID = int(item["from_id"].(float64))
+		}
+		wallPost.Date = int(item["date"].(float64))
+		wallPost.PostType = item["post_type"].(string)
+		wallPost.Text = item["text"].(string)
+
+		// если есть прикрепления, то вызываем парсер прикреплений
+		if mediaContent, exist := item["attachments"]; exist == true {
+			wallPost.Attachments = ParseAttachments(mediaContent.([]interface{}))
+		}
+
+		// если есть репосты, то вызываем парсер репостов
+		if copyHistory, exist := item["copy_history"]; exist == true {
+			wallPost.Attachments = append(wallPost.Attachments, parseCopyHistory(
+				copyHistory.([]map[string]interface{})))
+		}
+		wallPosts = append(wallPosts, wallPost)
+	}
+	return wallPosts
+}
+
+// MakeMessageWallPost собирает сообщение с данными о посте
+func makeMessageWallPost(sender string, subject Subject,
+	wallPostMonitorParam WallPostMonitorParam, wallPost WallPost) (string, error) {
+
+	// собираем данные для сигнатуры сообщения:
+	// тип поста
+	postType := fmt.Sprintf("%v", wallPost.PostType)
+
+	// где он был обнаружен
+	var locationHyperlink string
+	if wallPost.OwnerID < 0 {
+		vkCommunity, err := GetCommunityInfo(sender, subject, wallPost.OwnerID)
+		locationHyperlink = MakeCommunityHyperlink(vkCommunity)
+		if err != nil {
+			return "", err
+		}
+	} else {
+		vkUser, err := GetUserInfo(sender, subject, wallPost.OwnerID)
+		locationHyperlink = MakeUserHyperlink(vkUser)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	// кто автор, если это пользователь
+	var authorHyperlink string
+	if wallPost.FromID < 0 {
+		authorHyperlink = "[no_data]"
+	} else {
+		vkUser, err := GetUserInfo(sender, subject, wallPost.FromID)
+		authorHyperlink = MakeUserHyperlink(vkUser)
+		if err != nil {
+			return "", err
+		}
+	}
+
+	// и когда был создан
+	var creationDate string
+	creationDate = UnixTimeStampToDate(wallPost.Date + 18000) // цифру изменить под свой часовой пояс (тут 5 часов)
+
+	// формируем строку с прикреплениями
+	var attachments string
+	var link string
+	if len(wallPost.Attachments) > 0 {
+		for _, attachment := range wallPost.Attachments {
+			// отдельно проверим прикрепленную к посту ссылку
+			if attachment.Type == "link" {
+				link = attachment.URL
+			} else { // если не ссылка, значит - всё остальное
+				attachments = fmt.Sprintf("%v%d_%d", attachment.Type, attachment.OwnerID, attachment.ID)
+				if len(attachment.AccessKey) > 0 {
+					attachments += fmt.Sprintf("_%v", attachment.AccessKey)
+				}
+				// заранее добавляем запятую, чтобы следующее прикрепление не слилось к предыдущим
+				attachments += ","
+			}
+		}
+
+		// если кроме ссылки в прикреплениях было что-то еще, то нужно удалить лишнюю запятую в конце строки
+		if len(attachments) > 0 {
+			if string(attachments[len(attachments)-1]) == "," {
+				attachments = string(attachments[0 : len(attachments)-1])
+			}
+		}
+	}
+
+	// собираем ссылку на пост
+	postURL := fmt.Sprintf("https://vk.com/wall%d_%d", wallPost.OwnerID, wallPost.ID)
+
+	// добавляем подготовленные фрагменты сообщения в общий текст
+	// сначала сигнатуру
+	text := fmt.Sprintf("New %v\\nLocation: %v\\nAuthor: %v\\nCreated: %v",
+		postType, locationHyperlink, authorHyperlink, creationDate)
+
+	// затем основной текст поста, если он есть
+	if len(wallPost.Text) > 0 {
+		// но сначала экранируем все символы пропуска строки, потому что у json.Unmarshal с ними проблемы
+		wallPost.Text = strings.Replace(wallPost.Text, "\n", "\\n", -1)
+		text += fmt.Sprintf("\\n\\n%v", wallPost.Text)
+	}
+
+	// потом прикрепленную ссылку, если она есть
+	if len(link) > 0 {
+		if len(wallPost.Text) == 0 {
+			text += "\\n"
+		}
+		text += fmt.Sprintf("\\n%v", link)
+	}
+
+	// и ссылку на сам пост
+	text += fmt.Sprintf("\\n\\n%v", postURL)
+
+	// далее формируем строку с данными для карты
+	jsonDump := fmt.Sprintf(`{
+		"peer_id": "%d",
+		"message": "%v",
+		"v": "5.68"
+	`, wallPostMonitorParam.SendTo, text)
+
+	// если прикрепления были, то добавляем их в json-строку отдельно
+	if len(attachments) > 0 {
+		jsonDump += fmt.Sprintf(`, "attachment": "%v"`, attachments)
+	}
+
+	// закрываем карту
+	jsonDump += "}"
+
+	return jsonDump, nil
+}

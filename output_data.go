eb999a64d7f3a51286d392e908d94ca11fb793f0
VitJRBOG <vitjrbog@icloud.com>
2020-09-15 15:12:27 +0500

    Update - теперь программа может выводить в лог-файл весь путь до строки кода, на которой возникла ошибка; - из функции вывода сообщений программы в консоль удален вызов функции записи этих сообщений в лог-файл.

--- output_data.go
+++ output_data.go
@@ -2,31 +2,27 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 )
 
 // OutputMessage выводит сообщение в консоль
 func OutputMessage(sender string, message string) {
 	fmt.Printf("> [%v] [%v]: %v\n", UnixTimeStampToDate(int(time.Now().Unix())), sender, message)
-	OutputToTextFile(sender, message)
 }
 
-// OutputToTextFile сохраняет сообщение в текстовый файл
-func OutputToTextFile(sender string, message string) {
-	textToOutput := fmt.Sprintf("> [%v] [%v]: %v\n", UnixTimeStampToDate(int(time.Now().Unix())), sender, message)
+// ToLogFile сохраняет сообщение в текстовый файл
+func ToLogFile(errorMessage, trace string) {
+	textToOutput := fmt.Sprintf("[%v]: %v\n%v\n", UnixTimeStampToDate(int(time.Now().Unix())), errorMessage, trace)
 
 	logText, err := ReadTextFile("log.txt")
 	if err != nil {
-		date := UnixTimeStampToDate(int(time.Now().Unix()))
-		log.Fatal(fmt.Errorf("> [%v] WARNING! Error: %v", date, err))
+		panic(err.Error())
 	}
 
 	textToWrite := logText + textToOutput
 
 	err = WriteTextFile("log.txt", textToWrite)
 	if err != nil {
-		date := UnixTimeStampToDate(int(time.Now().Unix()))
-		log.Fatal(fmt.Errorf("> [%v] WARNING! Error: %v", date, err))
+		panic(err.Error())
 	}
 }

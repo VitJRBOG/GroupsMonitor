9603e202be2181f5ffde3939cbfb57b64d7cc961
VitJRBOG <vitjrbog@icloud.com>
2020-10-06 09:21:19 +0500

    Refactor - изменена структура модуля gui_thread_control.

--- gui_thread_control.go
+++ gui_thread_control.go
@@ -8,99 +8,112 @@ import (
 	"github.com/andlabs/ui"
 )
 
-// SubjectBoxData хранит данные для коробок с управленем потоками
-type SubjectBoxData struct {
-	Title  string
-	Button *ui.Button
-	Box    *ui.Box
+// boxThreadControl хранит данные о боксе для управления потоками мониторинга субъектов
+type boxThreadControl struct {
+	box                    *ui.Box
+	groupSubjSelectionBtns *ui.Group
+	groupThreadControlBtns *ui.Group
 }
 
+func (btc *boxThreadControl) init() {
+	btc.box = ui.NewHorizontalBox()
+	btc.box.SetPadded(true)
+	btc.groupSubjSelectionBtns = ui.NewGroup("")
+	btc.groupThreadControlBtns = ui.NewGroup("")
+	btc.box.Append(btc.groupSubjSelectionBtns, false)
+	btc.box.Append(btc.groupThreadControlBtns, true)
+}
+
+func (btc *boxThreadControl) setSubjSelectionBtnsBox(ssbb subjSelectingBtnsBox) {
+	btc.groupSubjSelectionBtns.SetChild(ssbb.box)
+}
+
+func (btc *boxThreadControl) setThreadControlBtnsBox(ssbd subjSelectingBtnData) {
+	btc.groupThreadControlBtns.SetChild(ssbd.box)
+}
+
+func (btc *boxThreadControl) setLabelNone() {
+	label := ui.NewLabel("Subjects not found")
+	btc.box.Append(label, true)
+}
+
+// subjSelectingBtnsBox хранит данные о боксе с кнопками для переключения между субъектами
+type subjSelectingBtnsBox struct {
+	box *ui.Box
+}
+
+func (ssbb *subjSelectingBtnsBox) init() {
+	ssbb.box = ui.NewVerticalBox()
+}
+
+func (ssbb *subjSelectingBtnsBox) appendButtons(listSSBD []subjSelectingBtnData) {
+	for i := 0; i < len(listSSBD); i++ {
+		ssbb.box.Append(listSSBD[i].button, false)
+	}
+}
+
+// subjSelectingBtnData хранит данные о кнопках для переключения между субъектами
+type subjSelectingBtnData struct {
+	title  string
+	button *ui.Button
+	box    *ui.Box
+}
+
+func (ssbd *subjSelectingBtnData) init(threads []*Thread, subjectName string) {
+	ssbd.title = subjectName
+	ssbd.button = ui.NewButton(subjectName)
+	ssbd.box = makeSubjectBox(subjectName, threads)
+}
+
+func (ssbd *subjSelectingBtnData) setFuncToBtn(listSSBD []subjSelectingBtnData, btc boxThreadControl) {
+	ssbd.button.OnClicked(func(*ui.Button) {
+		btc.groupThreadControlBtns.SetChild(ssbd.box)
+		btc.groupThreadControlBtns.SetTitle(ssbd.title)
+
+		for i := 0; i < len(listSSBD); i++ {
+			if !(listSSBD[i].button.Enabled()) {
+				listSSBD[i].button.Enable()
+			}
+		}
+		ssbd.button.Disable()
+	})
+}
+
+// makeThreadControlBox собирает бокс для контроля потоков мониторинга субъектов
 func makeThreadControlBox(threads []*Thread) *ui.Box {
-	// описываем коробку для управления потоками
-	boxThreadControl := ui.NewHorizontalBox()
-	boxThreadControl.SetPadded(true)
 
-	// в этом списке будут хранится данные о коробках, ориентированных на конкретные субъекты (сообщества)
-	var listSubjectBoxData []SubjectBoxData
+	subjectNames := getSubjectsNames()
 
-	// описываем коробку для кнопок переключения между субъектами
-	boxSubjectsSelection := ui.NewVerticalBox()
-	groupSubject := ui.NewGroup("")
+	var btc boxThreadControl
+	btc.init()
 
-	// получаем список названий субъектов
-	subjectsNames := getSubjectsNames()
-
-	// проверяем, если в готовом списке названий субъектов пусто
-	if len(subjectsNames) == 0 {
-		// то отображаем сообщение об этом в окне программы
-		labelNone := ui.NewLabel("Subjects not found")
-		boxThreadControl.Append(labelNone, true)
+	var listSSBD []subjSelectingBtnData
+	if len(subjectNames) > 0 {
+		for i := 0; i < len(subjectNames); i++ {
+			var ssbd subjSelectingBtnData
+			ssbd.init(threads, subjectNames[i])
+			listSSBD = append(listSSBD, ssbd)
+		}
 	} else {
-		// если список не пуст, перечисляем названия
-		for _, subjectName := range subjectsNames {
-			var subjectBoxData SubjectBoxData
-
-			// для начала создаем кнопку для отображения коробки управления данного конкретного субъекта
-			button := ui.NewButton(subjectName)
-			// затем добавляем название субъекта и ссылку на кнопку в структуру данных
-			subjectBoxData.Title = subjectName
-			subjectBoxData.Button = button
-
-			// и помещаем эту структуру в список
-			listSubjectBoxData = append(listSubjectBoxData, subjectBoxData)
-		}
-
-		// перечисляем структуры из ранее собранного списка
-		for i := 0; i < len(listSubjectBoxData); i++ {
-			// присваиваем данные в переменные, т.к. в методе OnClicked полученные извне индексы не работают
-			buttonTitle := listSubjectBoxData[i].Title
-			button := listSubjectBoxData[i].Button
-
-			// получаем получаем коробку, ориентированную на данный конкретный субъект
-			boxSubject := makeSubjectBox(buttonTitle, threads)
-
-			listSubjectBoxData[i].Box = boxSubject
-
-			// привязываем к кнопке отображения коробки управления субъекта соответствующую процедуру
-			listSubjectBoxData[i].Button.OnClicked(func(*ui.Button) {
-				groupSubject.SetChild(boxSubject)
-				groupSubject.SetTitle(buttonTitle)
-
-				for n := 0; n < len(listSubjectBoxData); n++ {
-					if !(listSubjectBoxData[n].Button.Enabled()) {
-						listSubjectBoxData[n].Button.Enable()
-					}
-				}
-
-				button.Disable()
-			})
-		}
-
-		// добавляем кнопки переключения между субъектами на коробку для этих кнопок
-		for _, subjectBoxData := range listSubjectBoxData {
-			boxSubjectsSelection.Append(subjectBoxData.Button, false)
-		}
-
-		// для выравнивания расположения кнопок относительно коробки управления потоками
-		// создаем группу для кнопок переключения между субъектами
-		groupSubjectsSelection := ui.NewGroup("")
-		// и добавляем на нее коробку с кнопками
-		groupSubjectsSelection.SetChild(boxSubjectsSelection)
-
-		// добавляем на коробку для управления потоками
-		// группу с кнопками для переключения между субъектами
-		boxThreadControl.Append(groupSubjectsSelection, false)
-		// и группу с коробками управления субъектами
-		boxThreadControl.Append(groupSubject, true)
-
-		// по умолчанию отображаем коробку первого в списке субъекта
-		groupSubject.SetChild(listSubjectBoxData[0].Box)
-		groupSubject.SetTitle(listSubjectBoxData[0].Title)
-		// и делаем неактивной кнопку отображения коробки этого субъекта
-		listSubjectBoxData[0].Button.Disable()
+		btc.setLabelNone()
 	}
 
-	return boxThreadControl
+	var ssbb subjSelectingBtnsBox
+	ssbb.init()
+
+	if len(listSSBD) > 0 {
+		for i := 0; i < len(listSSBD); i++ {
+			listSSBD[i].setFuncToBtn(listSSBD, btc)
+		}
+
+		ssbb.appendButtons(listSSBD)
+		btc.setSubjSelectionBtnsBox(ssbb)
+
+		btc.setThreadControlBtnsBox(listSSBD[0])
+		listSSBD[0].button.Disable()
+	}
+
+	return btc.box
 }
 
 func getSubjectsNames() []string {
@@ -121,99 +134,90 @@ func getSubjectsNames() []string {
 	return subjectsNames
 }
 
-func makeSubjectBox(subjectName string, threads []*Thread) *ui.Box {
-	// описываем коробку управления конкретным субъектом
-	boxSubject := ui.NewVerticalBox()
+// subjectBoxData хранит данные о боксе с кнопками для управления потоками мониторинга одного из субъектов
+type subjectBoxData struct {
+	box *ui.Box
+}
 
-	// создаем список с названиями модулей мониторинга (для названий кнопок управления потоками)
+func (sbd *subjectBoxData) init() {
+	sbd.box = ui.NewVerticalBox()
+}
+
+func (sbd *subjectBoxData) appendMonitorControlBoxes(listMCB []monitorControlBox) {
+	for i := 0; i < len(listMCB); i++ {
+		sbd.box.Append(listMCB[i].box, false)
+	}
+}
+
+// monitorControlBox хранит данные о кнопке для управления одним из потоков мониторинга субъекта
+type monitorControlBox struct {
+	box    *ui.Box
+	title  string
+	button *ui.Button
+	label  *ui.Label
+}
+
+func (mcb *monitorControlBox) init(buttonName string) {
+	mcb.box = ui.NewHorizontalBox()
+	mcb.title = buttonName
+	mcb.button = ui.NewButton(buttonName)
+	mcb.button.Disable()
+	mcb.label = ui.NewLabel("inactive")
+	mcb.box.Append(mcb.button, true)
+	mcb.box.Append(mcb.label, true)
+}
+
+func (mcb *monitorControlBox) setFuncToBtn(threadData *Thread) {
+	mcb.button.OnClicked(func(*ui.Button) {
+		if threadData.ActionFlag == 3 {
+			threadData.ActionFlag = 2
+		} else {
+			if threadData.ActionFlag != 1 {
+				threadData.ActionFlag = 3
+			}
+		}
+	})
+
+	mcb.button.Enable()
+	go threadStatusChecking(mcb.label, threadData)
+}
+
+// makeSubjectBox собирает бокс для управления потоками мониторинга одного из субъектов
+func makeSubjectBox(subjectName string, threads []*Thread) *ui.Box {
 	monitorsNames := []string{"Wall post monitoring", "Album photo monitoring", "Video monitoring",
 		"Photo comment monitoring", "Video comment monitoring", "Topic monitoring",
 		"Wall post comment monitoring"}
 
-	// в этом списке будут храниться кнопки для управления потоками
-	var btnsMonitorControl []*ui.Button
-	// а в этом - метки для отображения статусов соответствующих потоков
-	var lblsMonitorControl []*ui.Label
+	var listMCB []monitorControlBox
+	for i := 0; i < len(monitorsNames); i++ {
+		var mcb monitorControlBox
+		mcb.init(monitorsNames[i])
 
-	// перечисляем названия модулей мониторинга
-	for _, monitorName := range monitorsNames {
-		// описываем коробку для отображения кнопки управления и метки для статуса
-		boxMonitorControl := ui.NewHorizontalBox()
-
-		// описываем коробку для кнопки управления потоком
-		boxBtnMonitorControl := ui.NewVerticalBox()
-		// и описываем саму кнопку
-		btnMonitorControl := ui.NewButton(monitorName)
-		// затем добавляем на коробку для кнопки
-		boxBtnMonitorControl.Append(btnMonitorControl, false)
-		// по умолчанию делаем ее неактивной (делать активной будем потом)
-		btnMonitorControl.Disable()
-		// добавляем кнопку в список для кнопок
-		btnsMonitorControl = append(btnsMonitorControl, btnMonitorControl)
-		// а затем на коробку (сюда потом еще добавим метку для статуса)
-		boxMonitorControl.Append(boxBtnMonitorControl, true)
-
-		// описываем коробку для метки для статуса
-		boxLblMonitorControl := ui.NewVerticalBox()
-		// по умолчанию ставим статус inactive (менять статус будем потом)
-		lblMonitorControl := ui.NewLabel("inactive")
-		// затем добавляем на коробку для метки
-		boxLblMonitorControl.Append(lblMonitorControl, false)
-		// добавляем метку в списко для меток
-		lblsMonitorControl = append(lblsMonitorControl, lblMonitorControl)
-		// а затем на коробку, где ранее разместили кнопку управления потоком
-		boxMonitorControl.Append(boxLblMonitorControl, true)
-
-		// размещаем коробку с кнопкой и меткой на коробку управления потоками субъекта
-		boxSubject.Append(boxMonitorControl, false)
+		listMCB = append(listMCB, mcb)
 	}
 
-	// перечисляем описанные кнопки управления потоками
-	for i, btnMonitorControl := range btnsMonitorControl {
-		btnTitle := btnMonitorControl.Text()
-
-		// перечисляем данные о созданных потоках
-		for _, threadData := range threads {
-
-			// проверяем, содержится ли название субъекта в названии потока
-			if strings.Contains(strings.ToLower(threadData.Name), strings.ToLower(subjectName)) {
-
-				// сравниваем название потока с конкатенацией названия субъекта и названия кнопки
-				if strings.ToLower(threadData.Name) == strings.ToLower(subjectName+"'s "+btnTitle) {
-					// привязываем к кнопке управления потоком соответствующую процедуру
-					btnMonitorControl.OnClicked(func(*ui.Button) {
-						if threadData.ActionFlag == 3 {
-							threadData.ActionFlag = 2
-						} else {
-							if threadData.ActionFlag != 1 {
-								threadData.ActionFlag = 3
-							}
-						}
-					})
-					// делаем кнопку активной, так как поток, которым она управляет, существует
-					btnMonitorControl.Enable()
-
-					// проверку статуса данного потока мониторинга запускаем в отдельном потоке
-					go threadStatusChecking(lblsMonitorControl[i], threadData)
-
-					break
+	for i := 0; i < len(listMCB); i++ {
+		for n := 0; n < len(threads); n++ {
+			if strings.Contains(strings.ToLower(threads[n].Name), strings.ToLower(subjectName)) {
+				if strings.ToLower(threads[n].Name) == strings.ToLower(subjectName+"'s "+listMCB[i].title) {
+					listMCB[i].setFuncToBtn(threads[n])
 				}
 			}
 		}
 	}
 
-	return boxSubject
+	var sbd subjectBoxData
+	sbd.init()
+	sbd.appendMonitorControlBoxes(listMCB)
+
+	return sbd.box
 }
 
-func threadStatusChecking(lblMonitorControl *ui.Label, threadData *Thread) {
-	// запускаем вечный цикл
+func threadStatusChecking(statusLabel *ui.Label, threadData *Thread) {
 	for true {
-		// если текст в метке для отображения статуса не совпадает с названием статуса потока
-		if lblMonitorControl.Text() != threadData.Status {
-			// то меняем текст в метке на соответствующий названию статуса потока
-			lblMonitorControl.SetText(threadData.Status)
+		if statusLabel.Text() != threadData.Status {
+			statusLabel.SetText(threadData.Status)
 		}
-		// и ждем 1 секунду, затем повторяем цикл проверки
 		time.Sleep(1 * time.Second)
 	}
 }

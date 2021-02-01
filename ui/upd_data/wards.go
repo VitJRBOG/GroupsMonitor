eeae9f5165cffdad4bca19cf3bdb1b9793497d09
VitJRBOG <vitjrbog@icloud.com>
2021-02-01 15:08:30 +0500

    - пакет db: описана функция для добавления новой записи в таблицу ward; - пакет db: описана функция для получения записи из таблицы ward по ее name; - пакет db: описана функция для добавления новой записи в таблицу observer; - пакет data_manager: описаны функции для добавления нового Ward; - пакет data_manager: описаны функции для добавления нового Observer; - пакет ui/upd_data: описаны функции для получения от пользователя данных о новом подопечном; - пакет ui/upd_data: описаны функции для получения от пользователя данных о новых наблюдателях; - пакет ui: в обработчик команд пользователя добавлена обработка команды на добавление нового подопечного.

---
+++ ui/upd_data/wards.go
@@ -0,0 +1,110 @@
+package upd_data
+
+import (
+	"fmt"
+	"github.com/VitJRBOG/GroupsMonitor/data_manager"
+	"github.com/VitJRBOG/GroupsMonitor/tools"
+	"github.com/VitJRBOG/GroupsMonitor/ui/input"
+	"runtime/debug"
+	"strings"
+)
+
+func AddNewWard() {
+	activity := "Addition a new ward"
+
+	var ward data_manager.Ward
+	setWardName(&ward, activity)
+	setWardVkID(&ward, activity)
+	setWardGetAccessToken(&ward, activity)
+	ward.SaveToDB()
+
+	var w data_manager.Ward
+	err := w.SelectFromDB(ward.Name)
+	if err != nil {
+		tools.WriteToLog(err, debug.Stack())
+		panic(err.Error())
+	}
+	addNewObserver(&w)
+
+	fmt.Printf("[%s] %s: New ward added successfully...\n",
+		tools.GetCurrentDateAndTime(), activity)
+}
+
+func setWardName(ward *data_manager.Ward, activity string) {
+	fmt.Print("--- Enter a name for the new ward and press «Enter»... ---\n> ")
+	name := input.GetDataFromUser()
+	err := ward.SetName(name)
+	if err != nil {
+		switch true {
+		case strings.Contains(strings.ToLower(err.Error()), "string length is zero"):
+			fmt.Printf("[%s] %s: You must enter a name...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardName(ward, activity)
+			return
+		case strings.Contains(strings.ToLower(err.Error()), "ward with this name already exists"):
+			fmt.Printf("[%s] %s: A ward with this name already exists...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardName(ward, activity)
+			return
+		default:
+			tools.WriteToLog(err, debug.Stack())
+			panic(err.Error())
+		}
+	}
+}
+
+func setWardVkID(ward *data_manager.Ward, activity string) {
+	fmt.Print("--- Enter the VK ID for the new ward and press «Enter»... ---\n> ")
+	strVkID := input.GetDataFromUser()
+	err := ward.SetVkID(strVkID)
+	if err != nil {
+		switch true {
+		case strings.Contains(strings.ToLower(err.Error()), "string length is zero"):
+			fmt.Printf("[%s] %s: You must enter a VK ID...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardVkID(ward, activity)
+			return
+		case strings.Contains(strings.ToLower(err.Error()), "vk id starts with zero"):
+			fmt.Printf("[%s] %s: VK ID should not start with zero...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardVkID(ward, activity)
+			return
+		case strings.Contains(strings.ToLower(err.Error()), "invalid syntax"):
+			fmt.Printf("[%s] %s: VK ID must be integer...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardVkID(ward, activity)
+			return
+		case strings.Contains(strings.ToLower(err.Error()), "vk group id positive"):
+			fmt.Printf("[%s] %s: VK group ID must be negative...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardVkID(ward, activity)
+			return
+		default:
+			tools.WriteToLog(err, debug.Stack())
+			panic(err.Error())
+		}
+	}
+}
+
+func setWardGetAccessToken(ward *data_manager.Ward, activity string) {
+	fmt.Print("--- Enter the name of the access key to new ward and press «Enter»... ---\n> ")
+	accessTokenName := input.GetDataFromUser()
+	err := ward.SetAccessToken(accessTokenName)
+	if err != nil {
+		switch true {
+		case strings.Contains(strings.ToLower(err.Error()), "string length is zero"):
+			fmt.Printf("[%s] %s: You must enter a name of the access token...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardGetAccessToken(ward, activity)
+			return
+		case strings.Contains(strings.ToLower(err.Error()), "no such access token found"):
+			fmt.Printf("[%s] %s: an access token with this name does not exist...\n",
+				tools.GetCurrentDateAndTime(), activity)
+			setWardGetAccessToken(ward, activity)
+			return
+		default:
+			tools.WriteToLog(err, debug.Stack())
+			panic(err.Error())
+		}
+	}
+}

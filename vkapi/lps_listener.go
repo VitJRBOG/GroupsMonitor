7d9252129735d4c017806274c426025f37191e92
VitJRBOG <vitjrbog@icloud.com>
2021-02-26 10:13:44 +0500

    Bugfix
    Добавлены обработчики ошибок, которые ВК возвращает ответом на LongPoll-запрос вместо списка обновлений в сообществе.
    
    Ошибка «code 1: the event history is out of date or has been partially lost» случается, как правило, если использовать устаревший ts в LongPoll-запросе, поэтому для избежания этой ошибки достаточно увеличить значение ts. ВК предлагает в новом LongPoll-запросе использовать новый ts, который сервер возвращает вместе с кодом ошибки, но чтобы не пропустить никаких обновлений в группе, было решено просто увеличивать старый ts на 1 до тех пор, пока LongPoll-сервер не перестанет возвращать ошибки.
    
    Ошибка «code 2: key expired», как следует из описания в документации к VK API, означает, что передаваемый в LongPoll-запросе key устарел, поэтому для обработки данной ошибки достаточно просто запросить у сервера новый key и передать его в новом LongPoll-запросе.
    
    Ошибка «code 3: information lost», как следует из описания в документации к VK API, означает, что информация была утрачена, поэтому для обработки данной ошибки достаточно просто повторить отправку запроса. Чтобы не менять структуру функции ListenLongPollServer(), было решено также запрашивать и новые параметры для LongPoll-запроса.

--- vkapi/lps_listener.go
+++ vkapi/lps_listener.go
@@ -3,13 +3,14 @@ package vkapi
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/VitJRBOG/GroupsObserver/tools"
 	"io/ioutil"
 	"net/http"
 	"runtime/debug"
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/VitJRBOG/GroupsObserver/tools"
 )
 
 type ResponseLongPollServer struct {
@@ -26,7 +27,21 @@ func ListenLongPollServer(accessToken string, wardVkID, lastTS int) ResponseLong
 	lpsConnectionData := getLongPollServerConnectionData(accessToken, wardVkID)
 	queryForLongPollServer := makeQueryToLongPollServer(lpsConnectionData, lastTS)
 	response := sendRequestToLongPollServer(queryForLongPollServer)
-	respLPS := parseResponseLongPollServer(response)
+	respLPS, err := parseResponseLongPollServer(response)
+	if err != nil {
+		switch true {
+		case strings.Contains(strings.ToLower(err.Error()),
+			"the event history is out of date or has been partially lost"):
+			return ListenLongPollServer(accessToken, wardVkID, lastTS+1)
+		case strings.Contains(strings.ToLower(err.Error()), "key expired"):
+			return ListenLongPollServer(accessToken, wardVkID, lastTS)
+		case strings.Contains(strings.ToLower(err.Error()), "information lost"):
+			return ListenLongPollServer(accessToken, wardVkID, lastTS)
+		default:
+			tools.WriteToLog(err, debug.Stack())
+			panic(err.Error())
+		}
+	}
 	return respLPS
 }
 
@@ -62,12 +77,48 @@ func sendRequestToLongPollServer(url string) []byte {
 	return body
 }
 
-func parseResponseLongPollServer(response []byte) ResponseLongPollServer {
-	var respLPS ResponseLongPollServer
-	err := json.Unmarshal(response, &respLPS)
+func parseResponseLongPollServer(response []byte) (ResponseLongPollServer, error) {
+	var respValues map[string]interface{}
+
+	err := json.Unmarshal(response, &respValues)
 	if err != nil {
 		tools.WriteToLog(err, debug.Stack())
 		panic(err.Error())
 	}
-	return respLPS
+
+	var respLPS ResponseLongPollServer
+	if updates, exist := respValues["updates"]; exist {
+		respLPS.TS = respValues["ts"].(string)
+
+		for _, o := range updates.([]interface{}) {
+			item := o.(map[string]interface{})
+
+			var u UpdateFromLongPollServer
+			u.Type = item["type"].(string)
+			u.Object = item["object"].(map[string]interface{})
+
+			respLPS.Updates = append(respLPS.Updates, u)
+		}
+	} else {
+		if errorCode, exist := respValues["failed"]; exist {
+			var errorMsg string
+			switch errorCode.(float64) {
+			case 1:
+				errorMsg = "the event history is out of date or has been partially lost"
+			case 2:
+				errorMsg = "key expired"
+			case 3:
+				errorMsg = "information lost"
+			default:
+				errorMsg = "unknown error"
+			}
+
+			err = fmt.Errorf("code %v: %s", errorCode.(float64), errorMsg)
+			return respLPS, err
+		}
+		tools.WriteToLog(err, debug.Stack())
+		panic(err.Error())
+	}
+
+	return respLPS, nil
 }
